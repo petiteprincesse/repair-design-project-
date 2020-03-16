dbe9f3d62e380b1436d035b779097c8bfe91807b
Princess <Olgavm2007@yandex.ru>
2020-03-16 16:57:30 +0300

    feat: add section project and slider in this cection

--- src/js/main.js
+++ src/js/main.js
@@ -39,4 +39,23 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: 0}, 700);
     return false;
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 30 + bullets.width() + 20);
+  bullets.css('left', prev.width() + 30);
 });
