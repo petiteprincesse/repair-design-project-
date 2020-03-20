bc5ed9d5f6324bbecb2f254c469ef7e8164cb89f
Princess <Olgavm2007@yandex.ru>
2020-03-20 13:19:52 +0300

    feat: add steps block

--- src/js/main.js
+++ src/js/main.js
@@ -40,8 +40,13 @@ $(document).ready(function() {
     return false;
   });
 
+  /*
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
+    pagination: {
+      el: '.swiper-pagination-bull',
+      type: 'bullets',
+    },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
@@ -58,4 +63,101 @@ $(document).ready(function() {
 
   next.css('left', prev.width() + 30 + bullets.width() + 20);
   bullets.css('left', prev.width() + 30);
+  */
+
+ var mySwiper = new Swiper(".swiper-1", {
+  loop: true,
+
+  pagination: {
+    el: ".projects__pagination",
+    type: "bullets"
+  },
+
+  navigation: {
+    nextEl: ".projects__swiper-button-next",
+    prevEl: ".projects__swiper-button-prev"
+  },
+
+  slidesPerView: 1
+});
+
+var next = $(".projects__swiper-button-next");
+var prev = $(".projects__swiper-button-prev");
+var bullets = $(".projects__pagination");
+
+next.css("left", prev.width() + 21 + bullets.width() + 21 + prev.position().left);
+bullets.css("left", prev.width() + 21 + prev.position().left);
+
+var mySwiper2 = new Swiper(".swiper-2", {
+  effect: "fade",
+
+  pagination: {
+    el: ".steps__pagination",
+    type: "bullets"
+  },
+
+  controller: {
+    control: mySwiper3
+  },
+
+  fadeEffect: {
+    crossFade: true
+  }
+});
+
+var mySwiper3 = new Swiper(".swiper-3", {
+  effect: "fade",
+
+  pagination: {
+    el: ".steps__counter-pagination",
+    type: "fraction"
+  },
+
+  navigation: {
+    nextEl: ".steps__button-next",
+    prevEl: ".steps__button-prev"
+  },
+
+  controller: {
+    control: mySwiper2
+  },
+
+  fadeEffect: {
+    crossFade: true
+  }
+});
+
+var next = $(".steps__button-next");
+var prev = $(".steps__button-prev");
+var bullets = $(".steps__pagination");
+
+bullets.css("left", prev.width() + 21 + prev.position().left);
+next.css("left", prev.width() + 21 + bullets.width() + 21 + prev.position().left);
+
+$("#steps-button-0").click(function() {
+  mySwiper3.slideTo(0);
+});
+$("#steps-button-1").click(function() {
+  mySwiper3.slideTo(1);
+});
+$("#steps-button-2").click(function() {
+  mySwiper3.slideTo(2);
+});
+$("#steps-button-3").click(function() {
+  mySwiper3.slideTo(3);
+});
+$("#steps-button-4").click(function() {
+  mySwiper3.slideTo(4);
+});
+$("#steps-button-5").click(function() {
+  mySwiper3.slideTo(5);
+});
+
+mySwiper3.on("slideChange", function() {
+  var activeSlide = "#steps-button-" + mySwiper3.realIndex;
+  var prevSlide = "#steps-button-" + mySwiper3.previousIndex;
+
+  $(activeSlide).removeClass("inactive");
+  $(prevSlide).addClass("inactive");
+});
 });
