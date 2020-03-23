2f4c1f38ccf06473da2930c317fa80255c52042f
Princess <Olgavm2007@yandex.ru>
2020-03-23 20:20:01 +0300

    feat: add footer and form validate

--- src/js/main.js
+++ src/js/main.js
@@ -16,27 +16,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 */
 
 $(document).ready(function() {
-  var modal = $('.modal'), 
-    modalBtn = $('[data-toggle=modal]'), 
-    closeBtn = $('.modal__close');
+  var modal = $(".modal"),
+    modalBtn = $("[data-toggle=modal]"),
+    closeBtn = $(".modal__close");
 
-  modalBtn.on('click', function() {
-    modal.toggleClass('modal--visible'); 
+  modalBtn.on("click", function() {
+    modal.toggleClass("modal--visible");
   });
 
-  closeBtn.on('click', function() {
-    modal.toggleClass('modal--visible'); 
+  closeBtn.on("click", function() {
+    modal.toggleClass("modal--visible");
   });
 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $('.scrollup').toggleClass('.scrollup--visibility').fadeIn('slow'); 
+      $(".scrollup")
+        .toggleClass(".scrollup--visibility")
+        .fadeIn("slow");
     } else {
-      $('.scrollup').toggleClass('.scrollup--visibility').fadeOut('slow');
+      $(".scrollup")
+        .toggleClass(".scrollup--visibility")
+        .fadeOut("slow");
     }
   });
-  $('.scrollup').click(function() {
-    $('html, body').animate({scrollTop: 0}, 700);
+  $(".scrollup").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 700);
     return false;
   });
 
@@ -65,102 +69,138 @@ $(document).ready(function() {
   bullets.css('left', prev.width() + 30);
   */
 
- var mySwiper = new Swiper(".swiper-1", {
-  loop: true,
+  var mySwiper = new Swiper(".swiper-1", {
+    loop: true,
 
-  pagination: {
-    el: ".projects__pagination",
-    type: "bullets"
-  },
+    pagination: {
+      el: ".projects__pagination",
+      type: "bullets"
+    },
 
-  navigation: {
-    nextEl: ".projects__swiper-button-next",
-    prevEl: ".projects__swiper-button-prev"
-  },
+    navigation: {
+      nextEl: ".projects__swiper-button-next",
+      prevEl: ".projects__swiper-button-prev"
+    },
 
-  slidesPerView: 1
-});
+    slidesPerView: 1
+  });
 
-var next = $(".projects__swiper-button-next");
-var prev = $(".projects__swiper-button-prev");
-var bullets = $(".projects__pagination");
+  var next = $(".projects__swiper-button-next");
+  var prev = $(".projects__swiper-button-prev");
+  var bullets = $(".projects__pagination");
 
-next.css("left", prev.width() + 21 + bullets.width() + 21 + prev.position().left);
-bullets.css("left", prev.width() + 21 + prev.position().left);
+  next.css(
+    "left",
+    prev.width() + 21 + bullets.width() + 21 + prev.position().left
+  );
+  bullets.css("left", prev.width() + 21 + prev.position().left);
 
-var mySwiper2 = new Swiper(".swiper-2", {
-  effect: "fade",
+  var mySwiper2 = new Swiper(".swiper-2", {
+    effect: "fade",
 
-  pagination: {
-    el: ".steps__pagination",
-    type: "bullets"
-  },
+    pagination: {
+      el: ".steps__pagination",
+      type: "bullets"
+    },
 
-  controller: {
-    control: mySwiper3
-  },
+    controller: {
+      control: mySwiper3
+    },
 
-  fadeEffect: {
-    crossFade: true
-  }
-});
+    fadeEffect: {
+      crossFade: true
+    }
+  });
 
-var mySwiper3 = new Swiper(".swiper-3", {
-  effect: "fade",
+  var mySwiper3 = new Swiper(".swiper-3", {
+    effect: "fade",
 
-  pagination: {
-    el: ".steps__counter-pagination",
-    type: "fraction"
-  },
+    pagination: {
+      el: ".steps__counter-pagination",
+      type: "fraction"
+    },
 
-  navigation: {
-    nextEl: ".steps__button-next",
-    prevEl: ".steps__button-prev"
-  },
+    navigation: {
+      nextEl: ".steps__button-next",
+      prevEl: ".steps__button-prev"
+    },
 
-  controller: {
-    control: mySwiper2
-  },
+    controller: {
+      control: mySwiper2
+    },
 
-  fadeEffect: {
-    crossFade: true
-  }
-});
+    fadeEffect: {
+      crossFade: true
+    }
+  });
 
-var next = $(".steps__button-next");
-var prev = $(".steps__button-prev");
-var bullets = $(".steps__pagination");
+  var next = $(".steps__button-next");
+  var prev = $(".steps__button-prev");
+  var bullets = $(".steps__pagination");
 
-bullets.css("left", prev.width() + 21 + prev.position().left);
-next.css("left", prev.width() + 21 + bullets.width() + 21 + prev.position().left);
+  bullets.css("left", prev.width() + 21 + prev.position().left);
+  next.css(
+    "left",
+    prev.width() + 21 + bullets.width() + 21 + prev.position().left
+  );
 
-$("#steps-button-0").click(function() {
-  mySwiper3.slideTo(0);
-});
-$("#steps-button-1").click(function() {
-  mySwiper3.slideTo(1);
-});
-$("#steps-button-2").click(function() {
-  mySwiper3.slideTo(2);
-});
-$("#steps-button-3").click(function() {
-  mySwiper3.slideTo(3);
-});
-$("#steps-button-4").click(function() {
-  mySwiper3.slideTo(4);
-});
-$("#steps-button-5").click(function() {
-  mySwiper3.slideTo(5);
-});
+  $("#steps-button-0").click(function() {
+    mySwiper3.slideTo(0);
+  });
+  $("#steps-button-1").click(function() {
+    mySwiper3.slideTo(1);
+  });
+  $("#steps-button-2").click(function() {
+    mySwiper3.slideTo(2);
+  });
+  $("#steps-button-3").click(function() {
+    mySwiper3.slideTo(3);
+  });
+  $("#steps-button-4").click(function() {
+    mySwiper3.slideTo(4);
+  });
+  $("#steps-button-5").click(function() {
+    mySwiper3.slideTo(5);
+  });
 
-mySwiper3.on("slideChange", function() {
-  var activeSlide = "#steps-button-" + mySwiper3.realIndex;
-  var prevSlide = "#steps-button-" + mySwiper3.previousIndex;
+  mySwiper3.on("slideChange", function() {
+    var activeSlide = "#steps-button-" + mySwiper3.realIndex;
+    var prevSlide = "#steps-button-" + mySwiper3.previousIndex;
 
-  $(activeSlide).removeClass("inactive");
-  $(prevSlide).addClass("inactive");
-});
+    $(activeSlide).removeClass("inactive");
+    $(prevSlide).addClass("inactive");
+  });
 
-new WOW().init();
+  new WOW().init();
 
+  $(".modal__form").validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не должно быть короче двух букв"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+  });
+
+  $('[type=tel]').mask('+7 (000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+  
 });
